bf387989ee2992a8d526fdc1be995df0e4c05ccf
rodrock95 <r.barboza9502@gmail.com>
2021-07-16 19:07:12 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -33,4 +33,19 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	//atualizar um dado usuário
+	public User update(Long id, User obj) {
+		User entity = repository.getById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		entity.setName(obj.getName());
+	}
+	
 }
